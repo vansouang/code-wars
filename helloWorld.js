3df380d7a178e418d95ee116e797c5e49e59f12c
Samson Vansouang <svansouang@gmail.com>
2022-05-27 08:23:53 -0500

    Function that logs a string to the console

---
+++ helloWorld.js
@@ -0,0 +1,25 @@
+// mission 1:
+
+// use keyword function to define your function, function name should be helloWorld(don't forget the () and {})
+
+// mission 2:
+
+// use keyword var (or let or const) to define a variable str, value of str should be a string: "Hello World!"(don't forget the =).
+
+// mission 3:
+
+// type the console.log() in the next line(don't forget to put the str in the brackets).
+
+// function printWordToScreen(){
+//     var somewords="This is an example."
+//     console.log(somewords)
+//   }
+
+//create a function called hello world with no arguements
+//create a string variable "Hello World!"
+//console log the variable
+
+function helloWorld(){
+    let str = "Hello World!"
+    console.log(str)
+}
