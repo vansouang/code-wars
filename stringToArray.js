72cc7b1ca24e87c8fd9bb2c350be6807d75e5c6f
Samson Vansouang <svansouang@gmail.com>
2022-05-30 16:30:06 -0500

    Takes a string, returns each word in an array element

---
+++ stringToArray.js
@@ -0,0 +1,21 @@
+// Write a function to split a string and convert it into an array of words.
+
+// Examples (Input -> Output):
+// * "Robin Singh" ==> ["Robin", "Singh"]
+
+// * "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+
+//iterate through the array 
+//put each word into a separate element
+//split() takes a delimiter and puts each word into its own element
+
+function stringToArray(string){
+
+    newArray = string.split(" ")
+  
+    return newArray 
+  
+  }
+
+  //I think return string.split(" ") would work without using newArray variable
