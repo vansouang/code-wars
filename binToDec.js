1974fb94df8ce6802789d204a06e62559abbe3d5
Samson Vansouang <svansouang@gmail.com>
2022-06-09 08:27:33 -0500

    Converts a binary number to a decimal number

---
+++ binToDec.js
@@ -0,0 +1,7 @@
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+//convert to decimal number using parseInt()
+
+function binToDec(bin){
+    return parseInt(bin,2);
+  }
