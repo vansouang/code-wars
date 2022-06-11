267152e5913c1326cfbde33e0b69370ab2cb7607
Samson Vansouang <svansouang@gmail.com>
2022-06-11 10:09:49 -0500

    Returns suit based on second index of string

---
+++ defineSuit.js
@@ -0,0 +1,14 @@
+//conditional at the second index of the string to check for the shape
+//return shape based on the index
+
+
+function defineSuit(card) {
+    switch (card[1]){
+        case '♣': return 'clubs'
+        case '♦': return 'diamonds'
+        case '♥': return 'hearts'
+        case '♠': return 'spades'
+        
+    }
+      
+  }
