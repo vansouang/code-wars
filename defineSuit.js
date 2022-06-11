79f07b9ee6064369a9626d75cfd526c198e16c8a
Samson Vansouang <svansouang@gmail.com>
2022-06-11 14:35:16 -0500

    Fixed: switch case expression

--- defineSuit.js
+++ defineSuit.js
@@ -3,7 +3,7 @@
 
 
 function defineSuit(card) {
-    switch (card[1]){
+    switch (card[card.length-1]){
         case '♣': return 'clubs'
         case '♦': return 'diamonds'
         case '♥': return 'hearts'
