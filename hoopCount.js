c6cf36420a0b710672c6754e54078235f51f1d3b
Samson Vansouang <svansouang@gmail.com>
2022-06-26 19:44:46 -0500

    Returns a string based on the value passed in

---
+++ hoopCount.js
@@ -0,0 +1,8 @@
+function hoopCount (n) {
+    //test the value 
+   //if its 10 or greater return 'Great, now move on to tricks'
+   //if its less than 10 return 'Keep at it until you get it'
+   
+   if (n < 9) return "Keep at it until you get it"
+   else return "Great, now move on to tricks"
+ }
