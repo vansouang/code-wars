fb7251f7c3b48f3522666cb0dbd0ac093345852d
Samson Vansouang <svansouang@gmail.com>
2022-06-27 18:59:20 -0500

    Returns an array in multiples of the input

---
+++ countBy.js
@@ -0,0 +1,13 @@
+function countBy(x, n) {
+    let z = [];
+   
+    for (let i = 1; i <= n; i++){
+    
+      z.push(x*i)
+    }
+    return z;
+  }
+  
+  //create a function that loops n times
+  //inside the function we push the multiple of x each time
+  
