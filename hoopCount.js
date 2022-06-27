3e5c8a098c0fee80f4c8b36705ad16f4d0fbaf25
Samson Vansouang <svansouang@gmail.com>
2022-06-26 19:46:11 -0500

    Fixed: condition on hoopCount.js

--- hoopCount.js
+++ hoopCount.js
@@ -3,6 +3,6 @@ function hoopCount (n) {
    //if its 10 or greater return 'Great, now move on to tricks'
    //if its less than 10 return 'Keep at it until you get it'
    
-   if (n < 9) return "Keep at it until you get it"
+   if (n < 10) return "Keep at it until you get it"
    else return "Great, now move on to tricks"
  }
